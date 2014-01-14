edcea9d2c7e99017ff7566ecd0f2255e2066a561
berendscheffers <berend.scheffers.130@gmail.com>
2014-01-14 10:20:01 +0100

    first commit

---
+++ my-mavenschool/src/main/java/com/berendmaven/app/App.java
@@ -0,0 +1,13 @@
+package com.berendmaven.app;
+
+/**
+ * Berend
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hallo dit is maven van Berend!" );
+    }
+}
